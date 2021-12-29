6245392edd75b2cefc3eb23bd2dab522269ba14d
Alen Kvakic <alenkvakic@gmail.com>
2021-12-29 23:00:57 +0100

    implemented first test of api access only with fetch api for testing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  previewImages = []
 
   constructor() {}
+  
+  ngOnInit() {
+    fetch('https://www.reddit.com/r/pics/.json?&raw_json=1')
+      .then(res => res.json())
+      .then(res => {
+        console.log('res: ', res)
+        this.previewImages = res.data.children
+          .filter(entry => entry.data.preview)
+          .map(entry => entry.data.preview.images[0].source.url)
+          
+          console.log('this.previewImages: ', this.previewImages);
+      })
+  }
 
 }
