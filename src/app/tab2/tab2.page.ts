5d02d1ed4df6489daf833e6cecf48cc52cc5de60
Alen Kvakic <alenkvakic@gmail.com>
2022-01-11 19:56:32 +0100

    implemented aww and memes tab

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PostsService } from '../services/posts.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  previewImages$: Observable<unknown[]>
 
-  constructor() {}
+  constructor(private postsService: PostsService) {}
+  
+  ngOnInit() {
+    this.previewImages$ = this.postsService.getPosts('https://www.reddit.com/r/aww/.json?&raw_json=1')
+      .pipe(
+        map(res => {
+          return res['data'].children
+            .filter(entry => entry.data.preview)
+            .map(entry => entry.data.preview.images[0].source.url)
+        })
+      )
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PostsService } from '../services/posts.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  previewImages$: Observable<unknown[]>
 
-  constructor() {}
+  constructor(private postsService: PostsService) {}
+  
+  ngOnInit() {
+    this.previewImages$ = this.postsService.getPosts('https://www.reddit.com/r/memes/.json?&raw_json=1')
+      .pipe(
+        map(res => {
+          return res['data'].children
+            .filter(entry => entry.data.preview)
+            .map(entry => entry.data.preview.images[0].source.url)
+        })
+      )
+  }
 
 }
