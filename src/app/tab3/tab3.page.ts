5d4cebf56769c392e7292767ea971a61b1b11a10
Alen Kvakic <ak@webundsoehne.com>
2022-01-13 14:47:36 +0100

    adjusted view to use new view logic

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { RedditPost } from '../interfaces/reddit-post.interface';
@@ -10,27 +10,32 @@ import { PostsService } from '../services/posts.service';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit {
-  subredditPosts$: Observable<RedditPost[]>
-  selectedView = 'three-view'
-  showLabel = false;
-
   @ViewChild('content') content: any;
+  subredditPosts$: Observable<RedditPost[]>;
+  showLabel = false;
+  views = ['one', 'two', 'three', 'four'];
+  selectedView = 'three';
 
-  constructor(private postsService: PostsService) {}
-  
+  constructor(private postsService: PostsService) {
+  }
   ngOnInit() {
     this.subredditPosts$ = this.postsService.getPosts('https://www.reddit.com/r/memes/.json?&raw_json=1')
       .pipe(
-        map(res => {
-          return res['data'].children
+        map(res => res['data'].children
             .filter(entry => entry.data.preview)
-            .map(entry => entry.data)
-        })
-      )
+            .map(entry => entry.data))
+      );
   }
 
   switchDisplay() {
-    this.selectedView = this.selectedView === 'three-view' ? 'four-view' : 'three-view';
+    let selectedIndex = this.views.findIndex(entry => entry === this.selectedView);
+    const nextIndex = selectedIndex + 1;
+    if (nextIndex + 1 > this.views.length) {
+      selectedIndex = 0;
+    } else {
+      selectedIndex++;
+    }
+    this.selectedView = this.views[selectedIndex];
   }
 
   toggleLabel() {
@@ -38,13 +43,13 @@ export class Tab3Page implements OnInit {
   }
 
   menuOpened() {
-    this.content.el.classList.remove('front')
-    this.content.el.classList.add('back')
+    this.content.el.classList.remove('front');
+    this.content.el.classList.add('back');
   }
 
   menuClosed() {
-    this.content.el.classList.remove('back')
-    this.content.el.classList.add('front')
+    this.content.el.classList.remove('back');
+    this.content.el.classList.add('front');
   }
 
 }
